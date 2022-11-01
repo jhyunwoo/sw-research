7831b42855bad15a942a5a72d2e8a91b87f39d48
jhyunwoo <jhyunwoo0228@gmail.com>
2022-11-02 08:21:19 +0900

    add security

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true,  
+  // Adding policies:
+  async headers() {
+    return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: "camera=(); battery=(self); geolocation=()",
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+  },   
 }
 
 module.exports = nextConfig
