b3df0201ffabd360ff4b87eebfc40547c5dec35c
jhyunwoo <jhyunwoo0228@gmail.com>
2022-11-02 08:24:01 +0900

    delete security

--- next.config.js
+++ next.config.js
@@ -1,33 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,  
-  // Adding policies:
-  async headers() {
-    return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: "camera=(); battery=(self); geolocation=()",
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
-            },
-          ],
-        },
-      ];
-  },   
+  swcMinify: true,
 }
 
 module.exports = nextConfig
