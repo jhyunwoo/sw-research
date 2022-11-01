e042bd803d1cf3a4c71abce5c70d5fea2cc22b78
전현우 <jhyunwoo@Ampersand.local>
2022-11-02 00:24:31 +0900

    fix text error

--- pages/participants.js
+++ pages/participants.js
@@ -21,31 +21,31 @@ export default function Thanks() {
               <div className="mx-auto">주관</div>
             </div>
             <div className="grid grid-cols-3 gap-4 p-4 md:grid-cols-4 lg:grid-cols-5">
-              <div className="col-span-3 md:col-span-4 lg:col-span-5 p-2 bg-slate-100 rounded-lg flex justify-center flex justify-center">
+              <div className="col-span-3 md:col-span-4 lg:col-span-5 p-3 bg-slate-100 rounded-lg flex justify-center ">
                 CNSA IT 편집부
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 권하진
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 나우혁
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 이승주
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 전현우
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 정윤승
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 박유민
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 이은채
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 임진성
               </div>
             </div>
@@ -57,19 +57,19 @@ export default function Thanks() {
               <div className="mx-auto">사회</div>
             </div>
             <div className="grid grid-cols-3 gap-4 p-4 md:grid-cols-4 lg:grid-cols-5">
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 권하진
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 나우혁
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 이승주
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 전현우
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 정윤승
               </div>
             </div>
@@ -81,7 +81,7 @@ export default function Thanks() {
               <div className="mx-auto">포스터</div>
             </div>
             <div className="grid grid-cols-3 gap-4 p-4 md:grid-cols-4 lg:grid-cols-5">
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center">
                 박유민
               </div>
             </div>
@@ -93,7 +93,7 @@ export default function Thanks() {
               <div className="mx-auto">발표</div>
             </div>
             <div className="grid grid-cols-3 gap-4 p-4 md:grid-cols-4 lg:grid-cols-5">
-              <div className="col-span-3 md:col-span-4 lg:col-span-5 p-2 bg-slate-100 rounded-lg flex justify-center flex justify-center">
+              <div className="col-span-3 md:col-span-4 lg:col-span-5 p-3 bg-slate-100 rounded-lg flex justify-center ">
                 IT를 주제로 연구한 8기
               </div>
             </div>
@@ -106,26 +106,30 @@ export default function Thanks() {
               <div className="mx-auto">Special Thanks</div>
             </div>
             <div className="grid grid-cols-3 gap-4 p-4 md:grid-cols-4 lg:grid-cols-5">
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
                 김다인
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
                 김민서
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
                 민채은
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
                 윤기완
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
                 홍준혁
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
-                윤용철 선생님
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
+                윤용철
+                <br />
+                선생님
               </div>
-              <div className="p-2 bg-slate-100 rounded-lg flex justify-center">
-                이정석 선생님
+              <div className="p-3 bg-slate-100 rounded-lg flex justify-center items-center">
+                이정석
+                <br />
+                선생님
               </div>
             </div>
           </div>
