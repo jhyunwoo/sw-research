008174760ae8f6b0f50c5ebc9cd993cfae01377f
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-10 23:54:33 +0900

    fix footer error

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="pb-20 w-screen py-6">
       <div className="text-slate-500 text-center text-sm">
-        Copyright 전현우. All rights reserved expect posters.
+        Copyright 전현우. All rights reserved except posters.
       </div>
     </div>
   );
